5a4874767e7bcfc1163e9caf44fa06747c7fbe6f
Pavel <58663142+utkin-online@users.noreply.github.com>
2023-03-25 18:39:52 +0300

    First commit

---
+++ JavaScript ES6 - Основы/app.js
@@ -0,0 +1,96 @@
+// 1 Переменные
+// camelCase  написание с мБ (маленькабольшая)
+// var name = 'Programmer' // string
+// const lastName = 'Utkin'
+// const age = 40 // number
+// const isProgrammer = true //boolean
+
+// /const $ = 'some value'
+
+// console.log(firstName, lastName, age, isProgrammer)
+
+// const if ='mkef' //err
+// const withNum5 = '5'
+// const 5withNum5 = 5 //err
+
+
+// 2 Мутирование
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// const lastName = prompt('Введите свою Фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+// let currentYear = 2023
+// const birthYear = 1983
+
+// const age = currentYear + birthYear
+
+// const a = 10
+// const b = 5
+
+// let c = 32
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+// c += a
+// c -+ a
+// c *= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currentYear)
+// console.log(currentYear++)
+// console.log(--currentYear)
+// console.log(currentYear--)
+// console.log(c)
+
+// 4 Типы данных
+// const isProgrammer = true
+// const name = 'Pavel'
+// const age = 40
+// let x
+// null
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+
+// 5 Приоритет операторов
+// const fullAge = 39
+// const birthYear = 1983
+// const currentYear = 2023
+
+// > < >= <=
+// const isFullAge = currentYear - birthYear >= fullAge
+// console.log(isFullAge)
+
+// 6 Условные операторы
+// const courseStatus = 'fail' // ready, fail, pending
+
+// if (courseStatus === 'ready') {
+//     console.log('Курс уже готов и его можно проходить')
+// } else if (courseStatus === 'pending') {
+//     console.log('Курс пока находится в процессе разработки')
+// } else {
+//     console.log('Курс не получился')
+// }
+
+// const isReady = false
+
+// if (isReady === true) {
+//     console.log('Всё готово')
+// } else {
+//     console.log('Всё не готово!')
+// }
+// isReady = false
+// Тернарное выражение
+// isReady ? console.log('Всё готово') : console.log('Всё не готово!')
+
+// const num1 = 42 //number
+// const num2 = '42' //string
+
+// console.log(num1 == num2)
